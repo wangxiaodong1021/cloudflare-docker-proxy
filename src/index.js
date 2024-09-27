a11779b86d0b792de9d1b590c67277fbbec516d0
Wang Xiaodong <wangxiaodong1021@gmail.com>
2024-09-28 03:06:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.xdang.fun": dockerHub,
+  "quay.xdang.fun": "https://quay.io",
+  "gcr.xdang.fun": "https://gcr.io",
+  "k8s-gcr.xdang.fun": "https://k8s.gcr.io",
+  "k8s.xdang.fun": "https://registry.k8s.io",
+  "ghcr.xdang.fun": "https://ghcr.io",
+  "cloudsmith.xdang.fun": "https://docker.cloudsmith.io",
+  "ecr.xdang.fun": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
